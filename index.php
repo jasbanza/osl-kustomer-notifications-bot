a81646fecdaa445e5f6774a92317a6505bdcde1e
jasbanza <jason.banfield.za@gmail.com>
2022-06-17 23:43:23 +0200

    send response immediately to prevent webhook timeout.

--- index.php
+++ index.php
@@ -9,6 +9,37 @@ $json = file_get_contents('php://input');
 $data_json = json_decode($json, true);
 $data = [];
 
+// send response to prevent timeout on Kustomer's webhook end...
+
+// Buffer all upcoming output...
+ob_start();
+
+// Send your response.
+echo "Here be response";
+
+// Get the size of the output.
+$size = ob_get_length();
+
+// Disable compression (in case content length is compressed).
+header("Content-Encoding: none");
+
+// Set the content length of the response.
+header("Content-Length: {$size}");
+
+// Close the connection.
+header("Connection: close");
+
+// Flush all output.
+ob_end_flush();
+@ob_flush();
+flush();
+
+// Close current session (if it exists).
+if(session_id()) session_write_close();
+
+
+// NOW DO ACTUAL SCRIPTING:
+
 // request validation
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (strpos($_SERVER["CONTENT_TYPE"], "application/x-www-form-urlencoded") !== false) {
@@ -24,26 +55,55 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     die("Request failed. Expected 'REQUEST_METHOD' to be 'POST' or 'GET'.");
 }
 
-
-//
-// // authentication with secret
+// Check that it is from kustomer
 if (!isset($_SERVER["HTTP_X_KUSTOMER_WEBHOOK_EVENT"])) {
     die('Not from kustomer');
 }
 
 $conversationId = $data['data']['id'];
 
-tg_send_message("A new conversation has started!");
+// TEMP: this is stripped down to basics
+sleep(5);
 
-check_assigned($conversationId, 30, 10);
+$arrInfo = conversation_get_info($conversationId);
+if($arrInfo){
+  $txt = "<i>⭐ A new conversation has started!</i>";
+
+  // only include "admins" team
+  if($arrInfo['assignedTeam'] == "61c25e3a92453f62dccbcaaf"){
+    $isAssigned = false;
+    if($arrInfo['assignedUser']){
+      $isAssigned = true;
+      $displayName = get_displayName_from_userId($arrInfo['assignedUser']);
+      $txt = $txt."\nAgent: <b>".$displayName ."</b>";
+    }
+
+    $txt = $txt."\n\n<code>(".$conversationId.")</code>";
+    tg_send_message($txt);
+
+    if(!$isAssigned){
+      check_assigned($conversationId,15,10);
+    }
+  }
+}
+// end TEMP
+
+//check_assigned($conversationId, 30, 10);
 
 function check_assigned($conversationId, $numAttempts, $retryTimeout){
+  sleep($retryTimeout);
+  // get assigned user id
   $userId = conversation_get_assigned_userId($conversationId); // userId will be null if not assigned
+
+  // if assigned user id is found, proceed with name lookup
   if($userId){
     $displayName = get_displayName_from_userId($userId);
-    tg_send_message("... the conversation was assigned to: ".$displayName);
-  } else if($numAttempts > 0){
-    sleep($retryTimeout);
+    tg_send_message("👤 Assigned to: <b>".$displayName."</b>\n\n<code>(".$conversationId.")</code>");
+    exit;
+  }
+
+  // try again...
+  if($numAttempts > 0){
     check_assigned($conversationId,--$numAttempts, $retryTimeout);
   }
 }
@@ -74,6 +134,41 @@ function conversation_get_assigned_userId($conversationId){
     $userId = $arrResponse['data']['attributes']['assignedUsers'][0];
   }
   return $userId;
+}
+
+function conversation_get_info($conversationId){
+  global $config;
+  $ch = curl_init();
+  $url = 'https://api.kustomerapp.com/v1/conversations/'.$conversationId;
+
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Accept: application/json',
+    'Authorization: Bearer '.$config['KUSTOMER_API_KEY']
+  ));
+
+  $response = curl_exec($ch);
+  if (curl_errno($ch)) {
+      echo curl_error($ch);
+  }
+  curl_close($ch);
+
+  $arrResponse = json_decode($response, true);
+
+  $arrReturn = ["assignedUser" => null, "assignedTeam" => null]; // to return
+  if(isset($arrResponse['data']) && isset($arrResponse['data']['attributes'])){
+    // assigned user
+    if(isset($arrResponse['data']['attributes']['assignedUsers']) && isset($arrResponse['data']['attributes']['assignedUsers'][0])){
+      $arrReturn['assignedUser'] = $arrResponse['data']['attributes']['assignedUsers'][0];
+    }
+    // assigned team
+    if(isset($arrResponse['data']['attributes']['assignedTeams']) && isset($arrResponse['data']['attributes']['assignedTeams'][0])){
+      $arrReturn['assignedTeam'] = $arrResponse['data']['attributes']['assignedTeams'][0];
+    }
+  }
+  return $arrReturn;
 }
 
 function get_displayName_from_userId($userId){
@@ -114,7 +209,7 @@ function tg_send_message($msg_text){
   global $config;
 
   $telegram_chat_id = $config['TG_CHAT_ID'];
-  $url = 'https://api.telegram.org/bot'.$config['TG_BOT_TOKEN'].'/sendMessage';
+  $url = 'https://api.telegram.org/bot'.$config['TG_BOT_TOKEN'].'/sendMessage?parse_mode=html';
   $post_body = tg_bot_msg_post_body($telegram_chat_id, $msg_text);
 
   echo '<br><br>- Telegram bot: send message...';
